ef81f367483760ac641a9aadce01b80a0a609452
RasulDzuahunov <rasul.dju@gmail.com>
2024-12-26 18:12:11 +0600

    Add files via upload

---
+++ lessons43/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MainPage from "./page/mainPage/MainPage";
+
+function App() {
+  return (
+    <div>
+      <h1>Управление пользователями</h1>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
+
